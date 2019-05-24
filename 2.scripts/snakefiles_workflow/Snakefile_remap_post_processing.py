5debe1c29a13861314e2997f75bc6dd643e6027b
jcheneby <jeanne.cheneby@etu.univ-amu.fr>
2019-05-24 08:56:58 +0200

    working on sacapus post processing up to filtering. R not working...

--- 2.scripts/snakefiles_workflow/Snakefile_remap_post_processing.py
+++ 2.scripts/snakefiles_workflow/Snakefile_remap_post_processing.py
@@ -4,7 +4,7 @@ Affiliation: TAGC
 Aim: Workflow ReMap human
 Date: 01-12-16
 last update: 13-09-2018
-Run:snakemake --snakefile 2.scripts/snakefiles_workflow/Snakefile_remap_post_processing.py --printshellcmds --cores 10 --cluster-config config/sacapus.json --cluster "qsub -V -q {cluster.queue} -l nodes={cluster.node}:ppn={cluster.thread} -o {cluster.stdout} -e {cluster.stderr}" --keep-going --configfile config/Snakefile_config_remap_saccapus.json --use-conda
+Run : snakemake --snakefile 2.scripts/snakefiles_workflow/Snakefile_remap_post_processing.py --printshellcmds --cores 10 --cluster-config 2.scripts/cluster_configuration/cluster_conda_torque_remap_post_processing.json --cluster "qsub -V -q {cluster.queue} -l nodes={cluster.node}:ppn={cluster.thread} -o {cluster.stdout} -e {cluster.stderr}" --keep-going --configfile 2.scripts/snakemake_configuration/Snakefile_config_remap_sacapus_post_processing.json --use-cond
 
 dag: snakemake --snakefile 2.scripts/snakefiles_workflow/Snakefile_remap_post_processing.py --printshellcmds --cores 10 --cluster-config config/sacapus.json --cluster "qsub -V -q {cluster.queue} -l nodes={cluster.node}:ppn={cluster.thread} -o {cluster.stdout} -e {cluster.stderr}" --keep-going --configfile config/Snakefile_config_remap_saccapus.json --dag 2> /dev/null | dot -T svg > dag.svg
 rulegraph: snakemake --snakefile Snakefile_remap_v4.py --printshellcmds --cores 10 --cluster-config config/sacapus.json --cluster "qsub -V -q {cluster.queue} -l nodes={cluster.node}:ppn={cluster.thread} -o {cluster.stdout} -e {cluster.stderr}" --keep-going --configfile config/Snakefile_config_remap_saccapus.json --rulegraph 2> /dev/null | dot -T svg > rulegraph.svg
@@ -63,7 +63,7 @@ LIBRARY_MD5 = config["header"]["md5"]
 # include: os.path.join(BASE_DIR, RULE_DIR, "quality_FRiP.rules")
 # include: os.path.join(BASE_DIR, RULE_DIR, "quality_all.rules")
 
-include: os.path.join(BASE_DIR, RULE_DIR, "delete_trim.rules")
+#include: os.path.join(BASE_DIR, RULE_DIR, "delete_trim.rules")
 
 #================================================================#
 #                     Defining dataset                           #
@@ -88,18 +88,21 @@ for objects_indir in list_objects_indir: # loop through all the files and folder
 
 
             reader = csv.DictReader( csvfile, delimiter = "\t")
+            dict_experiment_chip_filename[ experiment_name] = []
             for row in reader:
 
                 current_filename = row[ FILENAME_HEADER].strip()
                 current_experiment_type = row[ CONTROL_HEADER].strip()
 
-                dict_experiment_chip_filename[ experiment_name] = []
 
-                if current_experiment_type == "1":
+
+                if current_experiment_type == "0":
                     dict_experiment_chip_filename[ experiment_name].append( current_filename)
 
 
-
+# print( list_exp)
+# pprint.pprint( dict_experiment_chip_filename)
+#print( expand( os.path.join( QUALITY_DIR, "macs2_{experiment_name}.quality_all"), experiment_name = list_exp))
 
 #================================================================#
 #                         Workflow                               #
@@ -107,7 +110,9 @@ for objects_indir in list_objects_indir: # loop through all the files and folder
 
 
 rule all:
-    input:  expand( os.path.join( QUALITY_DIR, "macs2_{experiment_name}.FRiP_NSC_RSC"), experiment_name = list_exp)
+    input:  #expand( os.path.join( QUALITY_DIR, "macs2_{experiment_name}.quality_all"), experiment_name = list_exp),
+            # os.path.join( QUALITY_DIR, "results", "macs2.quality_all")
+            os.path.join( QUALITY_DIR, "results", "macs2_passed.quality_all")
 
 
 
@@ -115,7 +120,8 @@ rule merge_bam:
     input:
             bam = lambda wildcards : expand( os.path.join( BAM_DIR, "{chip_name}.bam"), chip_name = dict_experiment_chip_filename[wildcards.experiment_name])
     output:
-            temp( os.path.join( PREPROCESSING_DIR,  "merge_bam", "{experiment_name}.bam"))
+            # temp( os.path.join( PREPROCESSING_DIR,  "merge_bam", "{experiment_name}.bam"))
+            os.path.join( PREPROCESSING_DIR,  "merge_bam", "{experiment_name}.bam")
     singularity:
             config[ "singularity"][ "samtools"]
     conda:
@@ -135,7 +141,8 @@ rule quality_NSC_RSC:
     input:
             bam = os.path.join( PREPROCESSING_DIR, "merge_bam", "{experiment_name}.bam")
     output:
-            temp( os.path.join( PREPROCESSING_DIR, "NSC_RSC", "{experiment_name}.NSC_RSC"))
+            # temp( os.path.join( PREPROCESSING_DIR, "NSC_RSC", "{experiment_name}.NSC_RSC"))
+            os.path.join( PREPROCESSING_DIR, "NSC_RSC", "{experiment_name}.NSC_RSC")
     singularity:
             config[ "singularity"][ "phantompeak"]
     conda:
@@ -148,25 +155,26 @@ rule quality_NSC_RSC:
             outdir = os.path.join( PREPROCESSING_DIR, "NSC_RSC"),
             run_spp = config[ "quality_NSC_RSC"][ "run_spp"]
     shell:"""
-    Rscript /usr/bin/phantompeakqualtools-1.2/run_spp.R -c={input.bam} -savp -out={output} -p=2 -odir={params.outdir} -rf {params.run_spp}
+    run_spp.R -c={input.bam} -savp -out={output} -p=2 -odir={params.outdir} -rf {params.run_spp}
     """
 
 
 
-rule quality_FRiP:
+rule quality_FRiP_nbPeaks:
     input:
             # bam = lambda wildcards : expand( os.path.join( BAM_DIR, "{chip_name}.bam"), chip_name = dict_experiment_chip_filename[wildcards.experiment_name]["chip"]),
             bam = os.path.join( PREPROCESSING_DIR, "merge_bam", "{experiment_name}.bam"),
             peaks = os.path.join( PEAKCALLING_DIR, "{experiment_name}", "macs2", "{experiment_name}_peaks.narrowPeak"),
             nsc_rsc = os.path.join( PREPROCESSING_DIR, "NSC_RSC", "{experiment_name}.NSC_RSC")
     output:
-            temp( os.path.join( PREPROCESSING_DIR, "FRiP", "macs2_{experiment_name}.FRiP"))
+            # temp( os.path.join( PREPROCESSING_DIR, "FRiP", "macs2_{experiment_name}.FRiP"))
+            os.path.join( PREPROCESSING_DIR, "FRiP", "macs2_{experiment_name}.FRiP")
     singularity:
             config[ "singularity"][ "bedtools"]
     conda:
             config[ "conda"][ "bedtools"]
     resources:
-            res=1
+            res=2
     log:
             os.path.join( PREPROCESSING_DIR, "FRiP", "log", "{experiment_name}_FRiP.log")
     params:
@@ -177,17 +185,20 @@ rule quality_FRiP:
 
         total_peaks_in_read=$(bedtools intersect {params.bedtools_intersect} -bed -u -a {input.bam} -b {input.peaks} | wc -l)
 
-        FRiP=$(awk -v nr="$total_reads" -v ni="$total_peaks_in_read" 'BEGIN{{print ni/nr * 100}}')
-        echo $FRiP > {output}
+        FRiP=$(awk -v nr="$total_reads" -v ni="$total_peaks_in_read" "BEGIN{{print ni/nr * 100}}")
+
+        total_peaks=$(wc -l < {input.peaks} )
+
+        echo $FRiP\t$total_peaks > {output}
     """
 
 
-rule quality_all:
+rule quality_all_experiment:
     input:
             nsc_rsc = os.path.join( PREPROCESSING_DIR, "NSC_RSC", "{experiment_name}.NSC_RSC"),
             frip = os.path.join( PREPROCESSING_DIR, "FRiP", "macs2_{experiment_name}.FRiP")
     output:
-            os.path.join( QUALITY_DIR, "macs2_{experiment_name}.FRiP_NSC_RSC")
+            os.path.join( QUALITY_DIR, "macs2_{experiment_name}.quality_all")
     resources:
             res=1
     log:
@@ -200,5 +211,55 @@ rule quality_all:
         NSC=$(cut -f9 {input.nsc_rsc})
         RSC=$(cut -f10 {input.nsc_rsc})
 
-        echo "experiment_name\tFRiP\tNSC\tRSC\n{params.exp_name}\t$FRiP\t$NSC\t$RSC\n" > {output}
+        echo "experiment_name\tNSC\tRSC\tFRiP\tnb_peaks\n{params.exp_name}\t$NSC\t$RSC\t$FRiP" > {output}
     """
+
+
+rule quality_all:
+    input:
+            expand( os.path.join( QUALITY_DIR, "macs2_{experiment_name}.quality_all"), experiment_name = list_exp)
+    output:
+            os.path.join( QUALITY_DIR,  "results", "macs2.quality_all")
+    resources:
+            res=1
+    log:
+            os.path.join( QUALITY_DIR, "log", "quality_all.log")
+    params:
+               indir = QUALITY_DIR,
+               outdir = os.path.join( QUALITY_DIR, "results")
+    shell:"""
+        mkdir -p {params.outdir}
+
+        cat {input} | awk "NR%2==0" | awk -F" " 'BEGIN {{print "experiment_name\tNSC\tRSC\tFRiP\tnb_peaks\tscore_NSC\tscore_RSC\tscore_FRiP\tscore_total" }}
+                                                  {{
+                                                    if( $2>=1.10) nsc=2; else if( $2>=1.05) nsc=1; else nsc=0;
+                                                    if( $3>=1) rsc=2; else if( $3>=0.8) rsc=1; else rsc=0;
+                                                    if( $4>=1) frip=1; else frip=0; score=nsc+rsc+frip; print $1"\t"$2"\t"$3"\t"$4"\t"$5"\t"nsc"\t"rsc"\t"frip"\t"score
+                                                  }}' >> {output}
+    """
+
+
+rule filtering_quality_all:
+    input:
+            os.path.join( QUALITY_DIR,  "results", "macs2.quality_all")
+    output:
+            passed = os.path.join( QUALITY_DIR,  "results", "macs2_passed.quality_all"),
+            failed = os.path.join( QUALITY_DIR,  "results", "macs2_failed.quality_all")
+    resources:
+            res=1
+    log:
+            os.path.join( QUALITY_DIR, "log", "quality_all.log")
+    params:
+               indir = QUALITY_DIR,
+               outdir = os.path.join( QUALITY_DIR, "results")
+    shell:"""
+
+         awk '{{
+                if( $5<100 || $9<2) {{ print > "{output.failed}"}}
+
+                else {{ print > "{output.passed}"}}
+
+              }}'  {input}
+
+    """
+#touch {output.passed} {output.failed}
